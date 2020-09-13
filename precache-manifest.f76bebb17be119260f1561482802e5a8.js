1548a77cbc57c6e4949ae968a3390bc38cbba3bd
jasonliang512 <jasonliang512@gmail.com>
2020-09-13 02:00:02 -0700

    Updates

---
+++ precache-manifest.f76bebb17be119260f1561482802e5a8.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "987da976a8b2a4169346a497ba84749d",
+    "url": "/heroicons-for-elm/favicon.ico"
+  },
+  {
+    "revision": "900d0f946f9fd88a63c4af764c68a072",
+    "url": "/heroicons-for-elm/index.html"
+  },
+  {
+    "revision": "6ead519905b7b907c00965be37dd5cd7",
+    "url": "/heroicons-for-elm/logo.svg"
+  },
+  {
+    "revision": "1c82903c2994c2507b385b9042bbfac5",
+    "url": "/heroicons-for-elm/manifest.json"
+  },
+  {
+    "revision": "3db0cc5bb8bff309eaa80e10e9a10d28",
+    "url": "/heroicons-for-elm/meta-image.png"
+  },
+  {
+    "revision": "06e701afa72bfa383b1f",
+    "url": "/heroicons-for-elm/static/css/main.7bce78b5.chunk.css"
+  },
+  {
+    "revision": "39c594365d6b6646ed91",
+    "url": "/heroicons-for-elm/static/css/vendors~main.d55a7715.chunk.css"
+  },
+  {
+    "revision": "06e701afa72bfa383b1f",
+    "url": "/heroicons-for-elm/static/js/main.06e701af.chunk.js"
+  },
+  {
+    "revision": "e3b644db3393feebd61a",
+    "url": "/heroicons-for-elm/static/js/runtime~main.e3b644db.js"
+  },
+  {
+    "revision": "39c594365d6b6646ed91",
+    "url": "/heroicons-for-elm/static/js/vendors~main.39c59436.chunk.js"
+  }
+]);
