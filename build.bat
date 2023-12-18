42b50f042b841cbbb6d484bb89c4b14a26874214
Jason Liang <jasonliang512@gmail.com>
2023-12-18 15:13:13 -0800

    Move some files

--- build.bat
+++ build.bat
@@ -1,6 +1,6 @@
 @echo off
 
-robocopy static dist /E
-robocopy resources dist/resources /E
+robocopy public dist /njh /njs /ndl /nc /ns
 tailwindcss -i src/style.css -o dist/style.css
-elm make src/Main.elm --optimize --output=dist/main.js
+elm make src/Main.elm --output=dist/main.js
+REM elm make src/Main.elm --optimize --output=dist/main.js
