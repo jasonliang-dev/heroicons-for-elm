2deb4ba4980d9a1f7656a32a345e43efd3c835a7
Jason Liang <jasonliang512@gmail.com>
2024-07-17 16:17:24 -0700

    Minify css

--- build.bat
+++ build.bat
@@ -1,6 +1,6 @@
 @echo off
 
 robocopy public dist /njh /njs /ndl /nc /ns
-tailwindcss -i src/style.css -o dist/style.css
+tailwindcss -i src/style.css -o dist/style.css --minify
 elm make src/Main.elm --output=dist/main.js
 REM elm make src/Main.elm --optimize --output=dist/main.js
